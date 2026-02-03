e91c11afed7058f945881572e752a45ce42a6cfe
SimranChaprana <simranchaprana77@gmail.com>
2026-02-03 18:01:23 +0000

    Sync LeetCode submission Runtime - 44 ms (34.55%), Memory - 47.2 MB (23.97%)

--- leetcode/0001-two-sum/solution.java
+++ leetcode/0001-two-sum/solution.java
@@ -1,24 +1,15 @@
 class Solution {
-    public static int[] twoSum(int[] nums, int target) {
-        // HashMap to store number and its index
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i]; // the other number needed
-            
-            if (map.containsKey(complement)) {
-                // found the pair
-                return new int[] { map.get(complement), i };
+    public int[] twoSum(int[] nums, int target) {
+         for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
             }
-            
-            // store current number with index
-            map.put(nums[i], i);
         }
-        
-        // if no solution found (though problem guarantees one)
-        throw new IllegalArgumentException("No two sum solution");
+         return new int[] {}; 
     }
-
-        
-    
+   
 }
+
+
