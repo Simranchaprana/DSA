d161a6ac5c46e636e9b875096ab677c749b7c2e9
SimranChaprana <simranchaprana77@gmail.com>
2026-06-10 16:02:28 +0000

    Sync LeetCode submission Runtime - 16 ms (70.74%), Memory - 46.9 MB (49.20%)

---
+++ leetcode/0459-repeated-substring-pattern/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+
+        for (int j = n/2; j >= 1; j--) {
+            if (n % j == 0) {
+                String sub = s.substring(0, j);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < n/j; i++) {
+                    sb.append(sub);
+                }
+                if (sb.toString().equals(s)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
+ 
